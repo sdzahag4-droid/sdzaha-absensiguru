8665555f7e68a0a94ec3e4ec17f739fb3d393d15
sdzahag4 <sdzahag4@gmail.com>
2026-08-09 12:03:56 +0700

    Update URL data rekap di rekap.js

--- js/rekap.js
+++ js/rekap.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyIreRt889UFmfNFBksAaj4Gq_WmUIsqaUpV6XeIvU-KGkKbJwfqaC13ZWV7mhiRiuG/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz4kpL6xEXXBkBO2aOGJGCKwFAYdk6Jwt0_8_WskgH8HvuECRg6MVIJlaxgDMAeIk_U/exec";
 
 function muatRekapAdmin() {
   const tbody = document.getElementById("tabel-rekap") || document.querySelector("tbody");
